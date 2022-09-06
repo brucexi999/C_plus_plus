afee35ef54527ae2ae1450b8e4d1c02e008fc6aa
Bruce Xi <91307696+brucexi999@users.noreply.github.com>
2022-09-05 22:44:41 -0700

    Create e1_4.cpp

---
+++ e1_4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main (void) {
+    int a, b;
+    std::cout << "Enter two numbers:" << std::endl;
+    std::cin >> a >>b; 
+    std::cout<<"The product of "<<a<<" and "<<b<<" is "<<a*b<<std::endl; 
+    return 0;
+}
