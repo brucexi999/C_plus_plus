f7dfa982ae0d32b984e1103c5633a258ac7e10e2
Bruce Xi <91307696+brucexi999@users.noreply.github.com>
2022-09-29 20:59:10 -0700

    Update middle_earth_class.cpp

--- middle_earth_class.cpp
+++ middle_earth_class.cpp
@@ -4,6 +4,8 @@
 using namespace std; // I am only using the std lib, so this line brings no problem. 
 
 class Elves {
+    private:
+        string kindred; 
     public: 
         int life_span = 999999999;
         int beauty = 10;
@@ -12,6 +14,7 @@ class Elves {
         Elves (string abirth_place) {
             birth_place = abirth_place; 
         }
+
         bool isCalaquendi (void) {
             if (birth_place == "Valinor") {
                 return true;
@@ -19,6 +22,19 @@ class Elves {
                 return false; 
             }
         }
+
+        void set_kindred (string akindred){
+            if (akindred == "Noldor" || akindred == "Vanyar" || akindred == "Teleri") {
+                kindred = akindred;
+            }
+            else kindred = "Unknown";
+        }
+
+        string get_kindred (void){
+            return kindred; 
+        }
+            
+
 }; 
 
 class Men {
@@ -66,5 +82,11 @@ int main (void) {
 
     cout << Gondolin.founder << endl;
     cout << Minas_Tirith.age << endl;
+
+    Luthien.set_kindred("Teleri"); 
+    Arwen.set_kindred("Mixed"); 
+
+    cout << Luthien.get_kindred() << endl; 
+    cout << Arwen.get_kindred() << endl; 
     return 0; 
 }
